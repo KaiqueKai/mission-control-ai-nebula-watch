89e6edc2184204748d8449f4cc316843ee4591a7
KaiqueKai <wkaiquekai@gmail.com>
2026-06-04 10:05:08 -0300

    Update mission_control_ai.py

--- mission_control_ai.py
+++ mission_control_ai.py
@@ -7,7 +7,7 @@
 # Link do vídeo pitch no YouTube:
 # https://www.youtube.com/watch?v=BzuYokZEfyU
 # Link do repostório do Github:
-#https://github.com/KaiqueKai/mission-control-ai-nebula-watch
+# https://github.com/KaiqueKai/mission-control-ai-nebula-watch
 
 # ============================================================
 # MISSION CONTROL AI
